6b21b13c5ab6295a5aff797c517d166d1cd00a46
yuk0ga <ukoooga@gmail.com>
2020-06-05 12:12:29 +1000

    refactor

--- src/01/index.js
+++ src/01/index.js
@@ -1,41 +1,48 @@
-// ページの読み込みを待つ
 window.addEventListener('load', init);
 
 function init() {
-
     // create a renderer
     const renderer = new THREE.WebGLRenderer({
         canvas: document.querySelector('#myCanvas')
     });
-    renderer.setSize(960, 540);
+    renderer.setSize(window.innerWidth, window.innerHeight); // width, height
 
     // create a scene
     const scene = new THREE.Scene();
 
     // create a camera
-    const camera = new THREE.PerspectiveCamera(45, 960 / 540);
-    camera.position.set(0, 0, +1500);
+    const camera = new THREE.PerspectiveCamera(
+        45, // field of view
+        window.innerWidth / window.innerHeight, // aspect ratio
+        1, // near clipping plane
+        2000 // far clipping plane
+        );
+    // anything beyond the near-far clipping plane 
+    // will not be displayed/calculated
+
+    camera.position.set(0, 200, 1500);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     // create a cube
-    const geometry = new THREE.BoxGeometry(500, 500, 500);
-
+    const geometry = new THREE.BoxGeometry(300, 300, 300);
     // define a material
     const material = new THREE.MeshNormalMaterial();
-
     // create a cube mesh with material
-    const box = new THREE.Mesh(geometry, material);
+    const cubeMesh = new THREE.Mesh(geometry, material);
+    cubeMesh.name = 'cube';
 
     // add cube to scene
-    scene.add(box);
+    scene.add(cubeMesh);
+    update(renderer, scene, camera);
+}
 
-    tick();
 
-    function tick() {
-        box.rotation.y += 0.1;
-        box.rotation.z -= 0.01;
+function update(renderer, scene, camera) {
+    var cube = scene.getObjectByName('cube');
+    cube.rotation.y += 0.05;
+    cube.rotation.z += 0.05;
 
-        renderer.render(scene, camera);
+    renderer.render(scene, camera);
 
-        requestAnimationFrame(tick);
-    }
+    requestAnimationFrame(() => update(renderer, scene, camera));
 }
