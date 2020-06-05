98db6360ad219f1fd2ad65afa6241b55e346fc97
yuk0ga <ukoooga@gmail.com>
2020-06-06 00:49:32 +1000

    use npm and webpack

--- src/01/index.js
+++ src/01/index.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three'
+
 window.addEventListener('load', init);
 
 function init() {
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mode: "development",
+
+    entry: "./src/01/index.js",
+
+    output: {
+        path: `${__dirname}/dist`,
+        filename: "main.js"
+    },
+} 
