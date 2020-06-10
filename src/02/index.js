143e15912ef01f0c070b5cd45ad4034fec31fcdf
Yu Koga <ukoooga@gmail.com>
2020-06-10 23:12:34 +1000

    add plane & fog

--- src/02/index.js
+++ src/02/index.js
@@ -10,7 +10,8 @@ function init() {
         canvas: document.querySelector('#myCanvas')
     });
     renderer.setSize(window.innerWidth, window.innerHeight); // width, height
-    
+    renderer.setClearColor('#ffffff');
+
     // create a scene
     const scene = new THREE.Scene();
     
@@ -28,6 +29,16 @@ function init() {
     
     camera.position.set(0, 200, 1500);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    scene.fog = new THREE.FogExp2('#ffffff', 0.001);
+
+    const cube = getCube(100, 100, 100);
+    scene.add(cube);
+
+    const plane = getPlane(500);
+    plane.position.y = - cube.geometry.parameters.height / 2
+    plane.rotation.x = Math.PI/2;
+    scene.add(plane);
     
     const controls = new OrbitControls(camera, renderer.domElement);
     update(renderer, scene, camera, controls);
@@ -39,4 +50,23 @@ function update(renderer, scene, camera, controls) {
 
     requestAnimationFrame(() => update(renderer, scene, camera, controls));
     
+}
+
+function getCube(width, height, depth) {
+    const geometry = new THREE.BoxGeometry(width, height, depth);
+    const material = new THREE.MeshBasicMaterial({
+        color: '#ff00ff'
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    return mesh;
+}
+
+function getPlane(size) {
+    const geometry = new THREE.PlaneGeometry(size, size, size);
+    const material = new THREE.MeshBasicMaterial({
+        color: '#12a12a',
+        side: THREE.DoubleSide
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    return mesh;
 }
