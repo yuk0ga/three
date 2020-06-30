37cc3bd53d346b2f66a120dc85e4d9ddcaef0d0e
Yu Koga <ukoooga@gmail.com>
2020-07-01 00:57:52 +1000

    add post processing

--- src/06/index.js
+++ src/06/index.js
@@ -1,6 +1,14 @@
 import * as THREE from 'three';
 import * as dat from 'dat.gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass';
+const vertexShader = require('./shaders/vertex.glsl');
+const fragmentShader = require('./shaders/fragment.glsl');
+var uMouse = new THREE.Vector2(0,0);
+var customPass;
 
 window.addEventListener('load', init);
 
@@ -26,28 +34,54 @@ function init() {
     // anything beyond the near-far clipping plane
     // will not be displayed/calculated
     
-    camera.position.set(0, 0, 2);
+    camera.position.set(0, 0, 1.5);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
     
-    const controls = new OrbitControls(camera, renderer.domElement);
+    // const controls = new OrbitControls(camera, renderer.domElement);
 
     let texture = new THREE.TextureLoader().load('../assets/images/taxi.jpg', (tex) => {
         tex.needsUpdate = true;
         mesh.scale.set(1.0, tex.image.height / tex.image.width, 1.0);
       }); 
+
     let mesh = new THREE.Mesh(
         new THREE.PlaneBufferGeometry(), 
         new THREE.MeshBasicMaterial({map: texture})
-    )
+    );
+
     scene.add(mesh);
 
-    update(renderer, scene, camera, controls);
+    let composer = new EffectComposer(renderer);
+    let renderPass = new RenderPass(scene, camera);
+
+    composer.addPass(renderPass);
+
+    let uniforms = {
+        "tDiffuse": { value: null },
+        "resolution": { value: new THREE.Vector2(1.,window.innerHeight/window.innerWidth) },
+        "uMouse": { value: new THREE.Vector2(-10,-10) },
+        "uVelo": { value: 0 },
+    }
+
+    customPass = new ShaderPass({uniforms, vertexShader, fragmentShader});
+
+    customPass.renderToScreen = true;
+    composer.addPass(customPass);
+
+    update(composer, scene, camera);
 }
 
-function update(renderer, scene, camera, controls) {
+document.addEventListener('mousemove', (e) => {
+    // mousemove / touchmove
+    uMouse.x = ( e.clientX / window.innerWidth ) ;
+    uMouse.y = 1. - ( e.clientY/ window.innerHeight );
+  });
 
-    renderer.render(scene, camera);
+function update(composer, scene, camera) {
+    customPass.uniforms.uMouse.value = uMouse;
 
-    requestAnimationFrame(() => update(renderer, scene, camera, controls));
+    composer.render();
+
+    requestAnimationFrame(() => update(composer, scene, camera));
     
 }
